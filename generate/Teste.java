de30b6bc5c9ba34d7b20f1fe90655d15cbee5df3
andrehideki <andre.hkiw@gmail.com>
2021-09-29 11:02:25 -0300

    add: JsonSchema2PojoConfig

---
+++ generate/Teste.java
@@ -0,0 +1,32 @@
+
+public class Teste {
+
+    private String foo;
+    private Integer bar;
+    private Boolean baz;
+
+    public String getFoo() {
+        return foo;
+    }
+
+    public void setFoo(String foo) {
+        this.foo = foo;
+    }
+
+    public Integer getBar() {
+        return bar;
+    }
+
+    public void setBar(Integer bar) {
+        this.bar = bar;
+    }
+
+    public Boolean getBaz() {
+        return baz;
+    }
+
+    public void setBaz(Boolean baz) {
+        this.baz = baz;
+    }
+
+}
--- src/test/java/br/com/jpamappergen/infra/JsonToPojoTest.java
+++ src/test/java/br/com/jpamappergen/infra/JsonToPojoTest.java
@@ -6,31 +6,31 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.jsonschema2pojo.DefaultGenerationConfig;
 import org.jsonschema2pojo.GenerationConfig;
+import org.jsonschema2pojo.NoopAnnotator;
+import org.jsonschema2pojo.SchemaGenerator;
 import org.jsonschema2pojo.SchemaMapper;
+import org.jsonschema2pojo.SchemaStore;
+import org.jsonschema2pojo.rules.RuleFactory;
 import org.junit.jupiter.api.Test;
 
 import com.sun.codemodel.JCodeModel;
+
+import br.com.jpamappergen.infra.jsonschema2pojo.JsonSchema2PojoConfig;
 
 public class JsonToPojoTest {
 
 	@Test
 	public void test() throws IOException {
 		JCodeModel codeModel = new JCodeModel();
-		URL source = getClass().getResource("/schema/required.json");
-		GenerationConfig config = new DefaultGenerationConfig() {
-			@Override
-			public boolean isGenerateBuilders() {
-				return false;
-			}
-		};
-
-//		SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new Jackson2Annotator(config), new SchemaStore()), new SchemaGenerator());
-		SchemaMapper mapper = new SchemaMapper();
+		URL source = getClass().getResource("/schema/teste.json");
+		GenerationConfig config = new JsonSchema2PojoConfig();		
+		SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new NoopAnnotator(), new SchemaStore()), new SchemaGenerator());
 		mapper.generate(codeModel, "ClassName", "", source);
 		Path destPath = Paths.get("generate");
-		Files.createDirectory(destPath);
+		if (!Files.exists(destPath)) {
+			Files.createDirectory(destPath);
+		}
 		codeModel.build(destPath.toFile());
 	}
 }
