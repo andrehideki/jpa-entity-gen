d0a8463e0354f388f1461c356f4a481aedf88ca7
andrehideki <andre.hkiw.dev@gmail.com>
2024-12-09 14:53:50 -0300

    change: change project framework to spring boot

--- src/main/java/br/com/jpamappergen/domain/service/ClassGenerator.java
+++
@@ -1,7 +0,0 @@
-package br.com.jpamappergen.domain.service;
-
-import java.util.List;
-
-public interface ClassGenerator {
-	public void generate(String className, List<ClassGeneratorProperty> properties);
-}
